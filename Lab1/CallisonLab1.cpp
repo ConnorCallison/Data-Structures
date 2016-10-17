d4ffd868beeb381024d70060d52c6034fc290801
Connor Callison <connorcallison@gmail.com>
2016-10-17 12:16:52 -0700

    Update CallisonLab1.cpp

--- Lab1/CallisonLab1.cpp
+++ Lab1/CallisonLab1.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 //This program creates a caeser cypher on an input string and given offset.
 
+//Encodes character based on ASCII value
 char encode_character(char character, int offset)
 {
     char newchar = character;
